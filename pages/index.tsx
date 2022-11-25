a73817a74393522b5d5dd71b2553c0ac2ded5f79
DraxyBearWD <w.hamilton@draxybearwd.com>
2022-11-26 01:56:54 +1000

    Feat: Added OpenGraph and FavIcon.

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,6 +24,12 @@ function Home({ session, loggedIn, username, email, id }: HomeProps) {
         <title>Xmas Apps</title>
         <meta name='description' content='Some fun Xmas Apps!' />
         <link rel='icon' href='/favicon.ico' />
+        <meta property='og:title' content='William Hamilton Xmas Apps' />
+        <meta property='og:description' content='Some Xmas apps I made.' />
+        <meta property='og:image' content='https://www.draxyz.com/og.png' />
+        <meta property='og:image:alt' content='Xmas Apps' />
+        <meta property='og:type' content='URL' />
+        <meta property='og:url' content='https://www.draxyz.com/' />
       </Head>
       <main className={styles.main}>
         <MenuItems />
